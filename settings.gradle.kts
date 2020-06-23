38701d9eaac7e9b453db444750d6debed0fc3eb3
Mikołaj Musidłowski <musidlo@gmail.com>
2020-06-23 11:14:52 +0200

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "rsq-zadanie-rekrutacyjne"
---
+++ src/main/kotlin/com/musidlowski/mikolaj/rsqzadanierekrutacyjne/RsqZadanieRekrutacyjneApplication.kt
@@ -0,0 +1,11 @@
+package com.musidlowski.mikolaj.rsqzadanierekrutacyjne
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RsqZadanieRekrutacyjneApplication
+
+fun main(args: Array<String>) {
+	runApplication<RsqZadanieRekrutacyjneApplication>(*args)
+}
